03c4238449e1eadc78d777585836a18e7866d2c1
v2n03 <shgudwns1515@naver.com>
2024-10-02 16:08:22 +0900

    initial commit

---
+++ week4.py
@@ -0,0 +1,11 @@
+# 두 정수 입력 받기
+num1 = int(input("첫 번째 정수를 입력하세요: "))
+num2 = int(input("두 번째 정수를 입력하세요: "))
+
+# 덧셈과 뺄셈 계산
+sum_result = num1 + num2
+sub_result = num1 - num2
+
+# 결과 출력
+print(f"두 수의 합: {sum_result}")
+print(f"두 수의 차: {sub_result}")
